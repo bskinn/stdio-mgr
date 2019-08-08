a5910c6f2eaaf6684e61edc6a928113ae0760af3
Brian Skinn <brian.skinn@gmail.com>
2019-08-08 12:30:54 -0400

    Recreate Azure config in one commit
    (1) TEST: Add initial trial azure-pipelines.yml
    
    Based on Hynek's post:
    https://hynek.me/articles/simple-python-azure-pipelines/
    
    (2) AZURE: Update to install req'ts
    
    Also disable install of special python for running tools;
    current setup just uses tools installed in-env.
    
    (3) AZURE: Try adding explicit PR build config
    
    Maybe this will trigger the autobuild on the PR, as opposed
    to a direct commit on master?
    
    (4) AZURE: Fix indentation error
    
    Maaaybe that indent level is important?
    
    (5) AZURE: Skip 3.8 for now
    
    3.8 not yet available as a default python version.
    
    (6) AZURE: Attempt switch to iterated Pythons
    
    One 'job' for each version of python
    
    (7) AZURE: Maybe 'variables', not 'parameters'?
    
    Looks like 'parameters' is for templates,
    'variables' is for the base YAML?
    
    (8) AZURE: Learning the substitution syntax...
    
    No markup around variables names within ${{}} iterations.
    
    (9) AZURE: Huh, no sequences in variables. :-/
    
    Maybe just using them directly is ok?
    
    (10) CI: Rename to reqts-ci.txt; try Azure template
    
    (11) AZURE: Fix (maybe) incorrect Python version spec
    
    (12) AZURE: Need templating syntax for python spec
    
    Have to use static/early substitution for parameters
    passed into the template!
    
    (13) AZURE: Try adding images
    
    Just linux and windows to start.
    
    (14) AZURE: Try 'images' as sequence, not mapping
    
    (15) AZURE: Pipeline worked; add macOs
    
    (16) AZURE: Add flake8 job
    
    (17) TESTS: Correct windows newlines in test asserts
    
    Also add an intentional flake8 failure to test that Azure job
    
    Will probably need to add logic to skip the README.rst doctesting on
    Windows.
    
    (18) AZURE: Try OS-specific pytest args
    
    Remove --doctest-glob from tox.ini config; attempt to selectively
    add the flag only for non-Windows builds.
    
    Also fix the spurious flake8 error from before
    
    (19) AZURE: Switch pytest args to var, not templated
    
    Since the script is defined outside of the jobs matrix iteration,
    the 'image' parameter value is not available. Further, the
    conditionals machinery of templating is not available for simple
    $[...] substitutions.
    
    So, the pytest args are moved into a variable, defined differently
    within the 'matrix:' for each OS type.
    
    (20) AZURE: Paren substitution for the variable?
    
    (21) AZURE: Don't use null for a null variable...
    
    ... at least, not one meant to be subtituted into a 'script:'
    
    It get inserted as the literal string "null".

--- tests/test_stdiomgr_base.py
+++ tests/test_stdiomgr_base.py
@@ -33,17 +33,17 @@ import warnings
 from stdio_mgr import stdio_mgr
 
 
-def test_capture_stdout():
+def test_capture_stdout(convert_newlines):
     """Confirm stdout capture."""
     with stdio_mgr() as (i, o, e):
         s = "test str"
         print(s)
 
         # 'print' automatically adds a newline
-        assert s + "\n" == o.getvalue()
+        assert convert_newlines(s + "\n") == o.getvalue()
 
 
-def test_capture_stderr():
+def test_capture_stderr(convert_newlines):
     """Confirm stderr capture."""
     with stdio_mgr() as (i, o, e):
         w = "This is a warning"
@@ -53,10 +53,10 @@ def test_capture_stderr():
             warnings.warn(w)
 
         # Warning text comes at the end of a line; newline gets added
-        assert w + "\n" in e.getvalue()
+        assert convert_newlines(w + "\n") in e.getvalue()
 
 
-def test_default_stdin():
+def test_default_stdin(convert_newlines):
     """Confirm stdin default-populate."""
     in_str = "This is a test string.\n"
 
@@ -67,13 +67,13 @@ def test_default_stdin():
 
         # TeeStdin tees the stream contents, *including* the newline,
         # to the managed stdout
-        assert in_str == o.getvalue()
+        assert convert_newlines(in_str) == o.getvalue()
 
         # 'input' strips the trailing newline before returning
-        assert in_str[:-1] == out_str
+        assert convert_newlines(in_str[:-1]) == out_str
 
 
-def test_managed_stdin():
+def test_managed_stdin(convert_newlines):
     """Confirm stdin populate within context."""
     str1 = "This is a test string."
     str2 = "This is another test string.\n"
@@ -82,14 +82,14 @@ def test_managed_stdin():
         # Preload str1 to stdout, and check. As above, 'print'
         # appends a newline
         print(str1)
-        assert str1 + "\n" == o.getvalue()
+        assert convert_newlines(str1 + "\n") == o.getvalue()
 
         # Use custom method .append to add the contents
         # without moving the seek position; check stdin contents.
         # The newline remains, since the stream contents were not
         # run through 'input'
-        i.append(str2)
-        assert str2 == i.getvalue()
+        i.append(convert_newlines(str2))
+        assert convert_newlines(str2) == i.getvalue()
 
         # Pull the contents of stdin to variable
         out_str = input()
@@ -97,18 +97,242 @@ def test_managed_stdin():
         # stdout should have both strings. The newline of str2 is
         # *retained* here, because str2 was teed from stdin upon
         # the read of stdin by the above 'input' call.
-        assert str1 + "\n" + str2 == o.getvalue()
+        assert convert_newlines(str1 + "\n" + str2) == o.getvalue()
 
         # 'input' should just have put str2 to out_str, *without*
         # the trailing newline, per normal 'input' behavior.
-        assert str2[:-1] == out_str
+        assert convert_newlines(str2[:-1]) == out_str
 
 
-def test_repeated_use():
+def test_repeated_use(convert_newlines):
     """Confirm repeated stdio_mgr use works correctly."""
     for _ in range(4):
         # Tests both stdin and stdout
-        test_default_stdin()
+        test_default_stdin(convert_newlines)
 
         # Tests stderr
-        test_capture_stderr()
+        test_capture_stderr(convert_newlines)
+
+
+def test_noop():
+    """Confirm state is restored after context."""
+    real_sys_stdio = (sys.stdin, sys.stdout, sys.stderr)
+    stdio_mgr()
+    assert (sys.stdin, sys.stdout, sys.stderr) == real_sys_stdio
+
+
+def test_exception():
+    """Confirm state is restored after an exception during context."""
+    real_sys_stdio = (sys.stdin, sys.stdout, sys.stderr)
+    with pytest.raises(ZeroDivisionError):
+        with stdio_mgr() as (i, o, e):
+            1 / 0
+    assert (sys.stdin, sys.stdout, sys.stderr) == real_sys_stdio
+
+
+def test_manual_close(convert_newlines):
+    """Confirm files remain open if close=False after the context has exited."""
+    with stdio_mgr(close=False) as (i, o, e):
+        test_default_stdin(convert_newlines)
+        test_capture_stderr(convert_newlines)
+
+    assert not i.closed
+    assert not o.closed
+    assert not e.closed
+
+    i.close()
+    o.close()
+    e.close()
+
+
+def test_manual_close_detached_fails(convert_newlines):
+    """Confirm files kept open become unusable after being detached."""
+    with stdio_mgr(close=False) as (i, o, e):
+        test_default_stdin(convert_newlines)
+        test_capture_stderr(convert_newlines)
+
+        i.detach()
+        o.detach()
+        e.detach()
+
+        with pytest.raises(ValueError) as err:
+            i.close()
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+        with pytest.raises(ValueError):
+            i.closed
+        with pytest.raises(ValueError):
+            o.close()
+        with pytest.raises(ValueError):
+            o.closed
+        with pytest.raises(ValueError):
+            e.close()
+        with pytest.raises(ValueError):
+            e.closed
+
+    with pytest.raises(ValueError) as err:
+        i.close()
+
+    assert str(err.value) == "underlying buffer has been detached"
+
+    with pytest.raises(ValueError):
+        i.closed
+    with pytest.raises(ValueError):
+        o.close()
+    with pytest.raises(ValueError):
+        o.closed
+    with pytest.raises(ValueError):
+        e.close()
+    with pytest.raises(ValueError):
+        e.closed
+
+
+def test_stdin_closed(convert_newlines):
+    """Confirm stdin's buffer can be closed within the context."""
+    with stdio_mgr() as (i, o, e):
+        print("test str")
+
+        i.close()
+
+        with pytest.raises(ValueError) as err:
+            i.getvalue()
+
+        assert str(err.value) == "I/O operation on closed file."
+
+        with pytest.raises(ValueError) as err:
+            i.append("anything")
+
+        assert str(err.value) == "I/O operation on closed file."
+
+        assert convert_newlines("test str\n") == o.getvalue()
+
+    assert convert_newlines("test str\n") == o.getvalue()
+
+
+def test_stdin_detached(convert_newlines):
+    """Confirm stdin's buffer can be detached within the context.
+
+    Like the real sys.stdin, use after detach should fail with ValueError.
+    """
+    with stdio_mgr() as (i, o, e):
+        print("test str")
+
+        f = i.detach()
+
+        with pytest.raises(ValueError) as err:
+            i.read()
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+        with pytest.raises(ValueError) as err:
+            i.getvalue()
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+        with pytest.raises(ValueError) as err:
+            i.append("anything")
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+        assert convert_newlines("test str\n") == o.getvalue()
+
+        print("second test str")
+
+        assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
+
+        with pytest.raises(ValueError) as err:
+            i.closed
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+    assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
+
+    assert not f.closed
+
+    with pytest.raises(ValueError) as err:
+        i.closed
+
+    assert str(err.value) == "underlying buffer has been detached"
+
+    assert o.closed
+    assert e.closed
+
+
+def test_stdout_detached(convert_newlines):
+    """Confirm stdout's buffer can be detached within the context.
+
+    Like the real sys.stdout, writes after detach should fail, however
+    writes to the detached stream should be captured.
+    """
+    with stdio_mgr() as (i, o, e):
+        print("test str")
+
+        f = o.detach()
+
+        assert isinstance(f, io.BufferedRandom)
+        assert f is o._buf
+        assert f is i.tee._buf
+
+        assert convert_newlines("test str\n") == o.getvalue()
+
+        with pytest.raises(ValueError) as err:
+            o.write("second test str\n")
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+        assert convert_newlines("test str\n") == o.getvalue()
+
+        with pytest.raises(ValueError) as err:
+            print("anything")
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+        f.write(convert_newlines("second test str\n").encode("utf8"))
+        f.flush()
+
+        assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
+
+        with pytest.raises(ValueError) as err:
+            o.closed
+
+        assert str(err.value) == "underlying buffer has been detached"
+
+    assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
+
+    assert not f.closed
+
+    with pytest.raises(ValueError) as err:
+        o.closed
+
+    assert str(err.value) == "underlying buffer has been detached"
+
+    assert i.closed
+    assert e.closed
+
+
+def test_stdout_access_buffer_after_close(convert_newlines):
+    """Confirm stdout's buffer is captured after close."""
+    with stdio_mgr() as (i, o, e):
+        print("test str")
+
+        assert convert_newlines("test str\n") == o.getvalue()
+
+        print("second test str")
+        o.close()
+
+        with pytest.raises(ValueError) as err:
+            o.read()
+
+        assert str(err.value) == "I/O operation on closed file."
+
+        assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
+
+        with pytest.raises(ValueError) as err:
+            print("anything")
+
+        assert str(err.value) == "I/O operation on closed file."
+
+        assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
+
+    assert convert_newlines("test str\nsecond test str\n") == o.getvalue()
